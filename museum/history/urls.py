2c74dd73915d2198a7669b3f9225da9398df0db8
egor_dikanskiy <egr.mr@yandex.ru>
2023-12-02 19:52:40 +0300

    history_detailed

--- museum/history/urls.py
+++ museum/history/urls.py
@@ -5,6 +5,6 @@ from history import views
 app_name = 'history'
 
 urlpatterns = [
-    path('', views.history),
-    path('history_detailed/', views.history_detailed)
+    path('', views.history, name='history'),
+    path('history_detailed/', views.history_detailed, name='history_detailed')
 ]
